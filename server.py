9ddd37cbed125f87f621004f0e06524b2e59a732
4zyz4 <133503200+4zyz4@users.noreply.github.com>
2023-05-14 12:41:32 +0800

    Update server.py

--- server.py
+++ server.py
@@ -6,5 +6,6 @@ while 1:
     os.system('clear')
     now=datetime.datetime.now()
     print(now.strftime("[%Y-%m-%d %H:%M:%S]"))
+    os.system('free')
     os.system('sudo /etc/init.d/shadowsocks status')
     time.sleep(1)
